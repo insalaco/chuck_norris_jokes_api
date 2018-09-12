81a526eace68e0863d8b43718f263afcaac9b265
Michael Insalaco <insalaco01@gmail.com>
2018-09-12 15:09:02 -0400

    assign variable to the value entered in the input field

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // call 'getJokes' function when click submit button
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  // assign variable 'number' to the value entered in the input field
+  const number = document.querySelector('input[type="number"]').value
+}
